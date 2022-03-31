c6291d8f026dd0a5db2074b11b3330fc62a13156
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-03-31 23:40:15 +0200

    feat: initial project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-footer elevated class="bg-dark">
+      <q-toolbar> </q-toolbar>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page id="desktop" class=""> </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Desktop',
+});
+</script>
+
+<style lang="scss">
+#desktop {
+  background-image: url('assets/wallpaper.jpg');
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+}
+</style>
