732e31aed1209b0e4731fe959988f24d5049c894
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-04-02 00:47:53 +0200

    feat: create app icons on desktop

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,51 @@
 <template>
-  <q-page id="desktop" class=""> </q-page>
+  <q-page id="desktop">
+    <div
+      id="desktop__apps"
+      class="column q-pa-md"
+      style="min-height: inherit; height: 600px"
+    >
+      <div
+        id="desktop__apps__app"
+        v-for="app in appsOnDesktop"
+        :key="app.id"
+        class="col-auto"
+      >
+        <q-btn flat no-caps>
+          <div class="column items-center">
+            <q-img :src="app.icon" height="48px" width="48px" />
+            <div class="text-weight-light">{{ app.name }}</div>
+          </div>
+        </q-btn>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useDesktop } from 'src/store/desktop';
+import { useApps } from 'src/store/app';
+import { Application } from 'src/models/app/Application';
 
 export default defineComponent({
   name: 'Desktop',
+  setup() {
+    const desktopStore = useDesktop();
+    const appsStore = useApps();
+
+    const appIdsOnDesktop = computed(() => desktopStore.getters.apps);
+    const appsOnDesktop = computed(
+      () =>
+        appIdsOnDesktop.value.map((appId) =>
+          appsStore.getters.apps.find((app) => app.id === appId)
+        ) as Application[]
+    );
+
+    return {
+      appsOnDesktop,
+    };
+  },
 });
 </script>
 
@@ -16,5 +55,14 @@ export default defineComponent({
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
+
+  &__apps {
+    gap: 16px;
+    align-content: flex-start;
+
+    &__app {
+      width: fit-content;
+    }
+  }
 }
 </style>
