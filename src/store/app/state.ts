bc62bacfffe539ff86805f83508851b9c98499ff
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-04-01 15:15:57 +0200

    feat: create first example application

--- src/store/app/state.ts
+++ src/store/app/state.ts
@@ -1,3 +1,11 @@
+import { Application } from 'src/models/app/Application';
+
 export default class AppsState {
-  public apps = [];
+  public apps: Application[] = [
+    new Application({
+      id: '49f95bc4-abd0-4297-b331-4c1e7557e5b6',
+      name: 'Terminal',
+      icon: '/apps/icons/terminal.png',
+    }),
+  ];
 }
