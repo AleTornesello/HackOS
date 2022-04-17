91183afe153cd238b71c026927734226542dce07
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-04-17 15:12:04 +0200

    fix: error when window position or size change

--- src/components/system/windows/WindowsManger.vue
+++ src/components/system/windows/WindowsManger.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="windows-container">
     <process-window
-      v-for="process in processes"
-      :key="process.id"
-      :process="process"
-      :data-app-id="process.application.id"
-      :data-x="lastAppWindowPositionByAppId(process.application.id).x"
-      :data-y="lastAppWindowPositionByAppId(process.application.id).y"
+      v-for="window in windows"
+      :key="window.process.id"
+      :process="window.process"
+      :data-process-id="window.process.id"
+      :data-app-id="window.process.application.id"
+      :data-x="window.position.x"
+      :data-y="window.position.y"
       :style="{
-        transform: `translate(${
-          lastAppWindowPositionByAppId(process.application.id).x
-        }px, ${lastAppWindowPositionByAppId(process.application.id).y}px)`,
-        width: `${lastAppWindowSizeByAppId(process.application.id).width}px`,
-        height: `${lastAppWindowSizeByAppId(process.application.id).height}px`,
+        transform: `translate(${window.position.x}px, ${window.position.y}px)`,
+        width: `${window.size.width}px`,
+        height: `${window.size.height}px`,
       }"
     >
     </process-window>
@@ -23,11 +22,18 @@
 import { InteractEvent, ResizeEvent } from '@interactjs/types';
 import interact from 'interactjs';
 import { useProcesses } from 'src/store/processes';
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, ref, watch } from 'vue';
 import ProcessWindow from 'src/components/system/windows/ProcessWindow.vue';
 import { useDesktop } from 'src/store/desktop';
 import { Position } from 'src/models/common/Position';
 import { Size } from 'src/models/common/Size';
+import { Process } from 'src/models/processes/Process';
+
+interface WindowItem {
+  process: Process;
+  position: Position;
+  size: Size;
+}
 
 export default defineComponent({
   name: 'WindowsManager',
@@ -38,13 +44,15 @@ export default defineComponent({
 
     const processes = computed(() => processesStore.getters.processes);
 
+    const windows = ref<WindowItem[]>([]);
+
     const lastAppWindowPositionByAppId = (appId: string): Position => {
-      return (
-        desktopStore.getters.lastAppWindowPositionByAppId(appId) || {
-          x: 0,
-          y: 0,
-        }
-      );
+      const position: Position | undefined =
+        desktopStore.getters.lastAppWindowPositionByAppId(appId);
+      return {
+        x: position ? position.x : 0,
+        y: position ? position.y : 0,
+      };
     };
 
     const lastAppWindowSizeByAppId = (appId: string): Size => {
@@ -56,50 +64,120 @@ export default defineComponent({
       );
     };
 
+    watch(
+      () => processes,
+      (newProcesses) => {
+        // Remove old windows
+        windows.value = windows.value.filter((window) =>
+          newProcesses.value.find((process) => process.id === window.process.id)
+        );
+
+        // Add new windows
+        newProcesses.value.forEach((process) => {
+          if (
+            !windows.value.find((window) => window.process.id === process.id)
+          ) {
+            const position: Position = {
+              x:
+                process.application && process.application.id
+                  ? lastAppWindowPositionByAppId(process.application.id).x + 30
+                  : 20,
+              y:
+                process.application && process.application.id
+                  ? lastAppWindowPositionByAppId(process.application.id).y + 30
+                  : 20,
+            };
+
+            const size: Size = {
+              width:
+                process.application && process.application.id
+                  ? lastAppWindowSizeByAppId(process.application.id).width
+                  : 300,
+              height:
+                process.application && process.application.id
+                  ? lastAppWindowSizeByAppId(process.application.id).height
+                  : 200,
+            };
+
+            windows.value.push({
+              process,
+              position,
+              size,
+            });
+
+            desktopStore.mutations.setWindowPosition({
+              appId: process.application.id as string,
+              newPosition: position,
+            });
+          }
+        });
+      },
+      { immediate: true, deep: true }
+    );
+
     const dragMoveListener = (event: InteractEvent) => {
       const target = event.target;
       // keep the dragged position in the data-x/data-y attributes
       const x = parseFloat(target.getAttribute('data-x') || '0') + event.dx;
       const y = parseFloat(target.getAttribute('data-y') || '0') + event.dy;
-      target.style.transform = `translate(${x}px, ${y}px)`;
-      // update the posiion attributes
-      target.setAttribute('data-x', x.toString(10));
-      target.setAttribute('data-y', y.toString(10));
-      // update the desktop store
-      desktopStore.mutations.setWindowPosition({
-        appId: target.getAttribute('data-app-id') as string,
-        newPosition: { x, y },
-      });
+      const processId = target.getAttribute('data-process-id');
+
+      if (processId) {
+        const processWindow = windows.value.find(
+          (window) => window.process.id === processId
+        );
+
+        if (processWindow) {
+          processWindow.position.x = x;
+          processWindow.position.y = y;
+        }
+
+        desktopStore.mutations.setWindowPosition({
+          appId: target.getAttribute('data-app-id') as string,
+          newPosition: { x, y },
+        });
+      }
     };
 
     const resizeListener = (event: ResizeEvent) => {
       const target = event.target;
       let x = parseFloat(target.getAttribute('data-x') || '0');
       let y = parseFloat(target.getAttribute('data-y') || '0');
-      // update the element's style
-      target.style.width = `${event.rect.width}px`;
-      target.style.height = `${event.rect.height}px`;
       // translate when resizing from top or left edges
       x += event.deltaRect?.left || 0;
       y += event.deltaRect?.top || 0;
-      target.style.transform = `translate(${x}px, ${y}px)`;
-      target.setAttribute('data-x', x.toString(10));
-      target.setAttribute('data-y', y.toString(10));
-      desktopStore.mutations.setWindowPosition({
-        appId: target.getAttribute('data-app-id') as string,
-        newPosition: { x, y },
-      });
-      desktopStore.mutations.setWindowSize({
-        appId: target.getAttribute('data-app-id') as string,
-        newSize: {
-          width: event.rect.width,
-          height: event.rect.height,
-        },
-      });
+
+      const processId = target.getAttribute('data-process-id');
+
+      if (processId) {
+        const processWindow = windows.value.find(
+          (window) => window.process.id === processId
+        );
+
+        if (processWindow) {
+          processWindow.position.x = x;
+          processWindow.position.y = y;
+          processWindow.size.width = event.rect.width;
+          processWindow.size.height = event.rect.height;
+        }
+
+        desktopStore.mutations.setWindowPosition({
+          appId: target.getAttribute('data-app-id') as string,
+          newPosition: { x, y },
+        });
+        desktopStore.mutations.setWindowSize({
+          appId: target.getAttribute('data-app-id') as string,
+          newSize: {
+            width: event.rect.width,
+            height: event.rect.height,
+          },
+        });
+      }
     };
 
     return {
       processes,
+      windows,
       lastAppWindowPositionByAppId,
       lastAppWindowSizeByAppId,
       dragMoveListener,
